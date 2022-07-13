edf7337f25e1f08787e32871dfe8729942fc727b
PatObate <patrick@nathanhr.com>
2022-07-13 19:41:22 +0800

    removed

--- src/containers/Blog/BlogsFirstSection/Data.js
+++
@@ -1,8 +0,0 @@
-export const BlogOne = {
-  highlight: true,
-  header: "Blogs",
-  menu1: "All",
-  menu2: "Travel",
-  menu3: "Visa",
-  menu4: "Freelance",
-};
--- src/service/addToRefs.js
+++
@@ -1,5 +0,0 @@
-export default function addToRefs(el, ref) {
-    if (el && !ref.current.includes(el)) {
-        ref.current.push(el);
-    }
-};
