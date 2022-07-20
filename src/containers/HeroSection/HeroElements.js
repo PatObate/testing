8237e48025db6279f12b3a8486d8e5666bf2cf6c
PatObate <patrick@nathanhr.com>
2022-07-20 14:55:07 +0800

    commit changes

--- src/containers/HeroSection/HeroElements.js
+++ src/containers/HeroSection/HeroElements.js
@@ -43,6 +43,7 @@ export const HeroContainer = styled(motion.div)`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    position: relative;
     padding: 10px 30px;
   }
 `;
